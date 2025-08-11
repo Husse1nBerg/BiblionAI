1d52224e49f1582bc28c110265bb2bf8a8d01723
Hussein <husseinbayoun@gmail.com>
2025-08-10 20:13:34 -0400

    Consolidate all dependencies into root package.json

--- server/server.js
+++ server/server.js
@@ -27,12 +27,14 @@ app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    // Serve static files from the React app
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+    // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
     });
 }
-
 
 // Start the server by listening on the specified port
 app.listen(PORT, () => {
